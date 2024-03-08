7a9f34a1250a3cd81e30c723d5316b1359621b4a
Yannick van Eck <yannickvaneck@MacBook-Pro-van-Yannick.local>
2024-03-08 09:18:38 +0100

    added lorem text to front page to see if the workflow works

--- wp-content/themes/CustomTheme/front-page.php
+++ wp-content/themes/CustomTheme/front-page.php
@@ -1,4 +1,4 @@
 <?php get_header();?>
 <h1>Wordpress With custom code testing site</h1>
-
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse veritatis eveniet excepturi. Ipsum dolores dolorum facilis. Ut iste earum eum, totam, illum quibusdam necessitatibus pariatur voluptas similique repellendus voluptatum. Sed.</p>
 <?php get_footer();?>
