b56f84823cffe661eed89d628c9db00561e93049
Yannick van Eck <yannickvaneck@MacBook-Pro-van-Yannick.local>
2024-03-11 16:15:23 +0100

    carousel on homepage

--- wp-content/themes/CustomTheme/front-page.php
+++ wp-content/themes/CustomTheme/front-page.php
@@ -1,4 +1,37 @@
 <?php get_header();?>
 <h1>Wordpress With custom code testing site</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse veritatis eveniet excepturi. Ipsum dolores dolorum facilis. Ut iste earum eum, totam, illum quibusdam necessitatibus pariatur voluptas similique repellendus voluptatum. Sed.</p>
+<?php
+$argsMovie = array(  
+    'post_type' => 'movie',
+    'posts_per_page' => '99',
+    'orderby' => 'title',
+    'order' => 'ASC'
+  );
+  $movies = new WP_Query($argsMovie);
+
+?>
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+  <?php
+            if($movies->have_posts()): 
+            while($movies->have_posts()): $movies->the_post();
+                $img = get_field('thumbnail');
+            ?>
+    <div class="carousel-item active">
+      <img src="<?php echo esc_url($img['url'])  ?>" class="d-block h-75 w-75" alt="<?php echo $title ?>">
+    </div>
+    <?php endwhile;
+            wp_reset_postdata(); // Reset post data after the movies loop
+            endif;
+            ?>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 <?php get_footer();?>
