f1d9781a3691d9a62e074b94a8ceba90b43539fa
Yannick van Eck <yannickvaneck@MacBook-Pro-van-Yannick.local>
2024-03-12 08:45:20 +0100

    deleted carousel on homepage

--- wp-content/themes/CustomTheme/front-page.php
+++ wp-content/themes/CustomTheme/front-page.php
@@ -10,28 +10,6 @@ $argsMovie = array(
   $movies = new WP_Query($argsMovie);
 
 ?>
-<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-  <?php
-            if($movies->have_posts()): 
-            while($movies->have_posts()): $movies->the_post();
-                $img = get_field('thumbnail');
-            ?>
-    <div class="carousel-item active">
-      <img src="<?php echo esc_url($img['url'])  ?>" class="d-block h-75 w-75" alt="<?php echo $title ?>">
-    </div>
-    <?php endwhile;
-            wp_reset_postdata(); // Reset post data after the movies loop
-            endif;
-            ?>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+
 </div>
 <?php get_footer();?>
---
+++ wp-content/themes/CustomTheme/page-onepager.php
@@ -0,0 +1,9 @@
+<?php 
+    get_header();
+
+    ?>
+
+<h1 class="display-1">This is where a One Pager is being built.</h1>
+<?php
+    get_footer();
+?>
