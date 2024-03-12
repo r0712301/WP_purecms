73202af473a1810457c092cb9512a9d2f2bd93a4
Yannick van Eck <yannickvaneck@MacBook-Pro-van-Yannick.local>
2024-03-12 09:37:26 +0100

    Added review to homepage for actors

--- wp-content/themes/CustomTheme/front-page.php
+++ wp-content/themes/CustomTheme/front-page.php
@@ -1,3 +1,4 @@
+<?php acf_form_head(); ?>
 <?php get_header();?>
 <h1>Wordpress With custom code testing site</h1>
 <?php
@@ -11,5 +12,19 @@ $argsMovie = array(
 
 ?>
 
+<div id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+    <?php while ( have_posts() ) : the_post(); ?>
+    <?php acf_form(array(
+        'post_id'       => 'new_post',
+        'new_post'      => array(
+            'post_type'     => 'review',
+            'post_status'   => 'draft'
+        ),
+        'submit_value'  => 'Review opladen'
+    )); ?>
+    <?php endwhile; ?>
+    </div><!-- #content -->
+</div><!-- #primary -->
 </div>
 <?php get_footer();?>
