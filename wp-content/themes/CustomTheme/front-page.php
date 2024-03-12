27c9b1dbba8081ce8a06700f51a699d1ee468bc2
Yannick van Eck <yannickvaneck@MacBook-Pro-van-Yannick.local>
2024-03-12 11:50:25 +0100

    Updated ACF pro

--- wp-content/themes/CustomTheme/front-page.php
+++ wp-content/themes/CustomTheme/front-page.php
@@ -15,17 +15,7 @@ $argsMovie = array(
 <div id="primary" class="content-area">
     <div id="content" class="site-content" role="main">
    
-    <?php acf_form(array(
-        'post_id'       => 'new_post',
-        'post_title' => 'new_post',
-        'form' => true,
-        'new_post'      => array(
-            'post_type'     => 'review',
-            'post_status'   => 'draft'
-        ),
-        'field_groups' => array(70),
-        'submit_value'  => 'Review opladen'
-    )); ?>
+    <?php acfe_form('review-actor'); ?>
 
     </div><!-- #content -->
 </div><!-- #primary -->
